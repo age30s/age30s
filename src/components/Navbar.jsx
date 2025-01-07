6ed23a74033b360ed3882cfbbfddde1fbac820d6
age30s <adithprasad@gmail.com>
2025-01-07 05:31:27 -0600

    g

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ export default function Navbar(){
         <Link to={"/age30s"} style={{ color: "#fb3f00", textDecoration: "none" }}>
             <span className="nav-item">Home</span>
         </Link>
-        <Link to={"/experiences"}  style={{ color: "#fb3f00", textDecoration: "none" }}>
+        <Link to={"/age30s/experiences"}  style={{ color: "#fb3f00", textDecoration: "none" }}>
             <span className="nav-item">Experiences</span>
         </Link>
             {/* <Link to={item.path}> */}
--- src/main.jsx
+++ src/main.jsx
@@ -15,7 +15,7 @@ const router = createBrowserRouter([
     element: <App/>,
   },
   {
-    path: "/experiences",
+    path: "/age30s/experiences",
     element: <Experiences/>,
   },
 ]); 
