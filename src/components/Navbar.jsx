820e3e4b6a52aa720aad3638f6785db84de203f6
age30s <adithprasad@gmail.com>
2025-01-07 10:22:49 -0600

    prog

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,8 @@ export default function Navbar(){
         <Link to={"/"} style={{ color: "#fb3f00", textDecoration: "none" }}>
             <span className="nav-item">Home</span>
         </Link>
-        <Link to={"/experiences"}  style={{ color: "#fb3f00", textDecoration: "none" }}>
+        {/* to={"/experiences"}  */}
+        <Link style={{ color: "#fb3f00", textDecoration: "none" }}>
             <span className="nav-item">Experiences</span>
         </Link>
             {/* <Link to={item.path}> */}
