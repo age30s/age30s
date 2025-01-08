bd0eb98b992fad22b8f4a43eb7fbc05b93c4ade4
age30s <adithprasad@gmail.com>
2025-01-08 12:35:53 -0600

    adjusting txt

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -22,7 +22,7 @@ function Home() {
         <h2>CS major + Finance minor at the <b className='highlight'> University of Illinois at Chicago</b></h2>
       </div>
       <p className='p-cl'>
-        Hi, I’m Adithya, a computer science major with a finance minor at the University of Illinois Chicago. I’m passionate about leveraging technology for social good and exploring innovative solutions at the intersection of these fields. My current interests include large-scale data processing, wearable technology, and crafting engaging, interactive user experiences
+        Hi, I’m Adithya, a computer science major with a finance minor at the University of Illinois Chicago. I’m passionate about leveraging technology for social good and exploring innovative solutions for global issues. My current interests include large-scale data processing, wearable technology, and crafting engaging, interactive user experiences
       </p>
       </div>
     </div>
