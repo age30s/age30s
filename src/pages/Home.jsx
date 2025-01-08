ed71fda9b67ff39718a83d2e86e7114c709b5909
age30s <adithprasad@gmail.com>
2025-01-08 02:32:36 -0600

    fixing cross platform func

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,8 @@
 import "../App.css"
 import Navbar from "../components/Navbar.jsx"
-import Pic from "../assets/me.png"
+// import Pic from "../assets/me.png"
 import Footer from "../components/Footer.jsx"
+import Pic from "../assets/pngme.png"
 
 function Home() {
 
@@ -11,22 +12,18 @@ function Home() {
     <Navbar/>
     <div className='cont'>
       <div className='container'>
-      <div className='backdrop2'>
-      </div>
-        <div className='backdrop'>
-        </div>
         <div>
-          <img src={Pic} className="img" alt="me" />
+          <img src={Pic} className="pic" alt="me" />
         </div>
       </div>
       <div>
       <div className='text'>
         <h1>Adithya Prasad</h1>
-        <h5>CS major + Finance minor at the <b className='highlight'> University of Illinois at Chicago</b></h5>
+        <h2>CS major + Finance minor at the <b className='highlight'> University of Illinois at Chicago</b></h2>
       </div>
       <p className='bg'>
-        Hi there! I'm Adithya, a computer science major minoring in finance at the University of Illinois at Chicago.
-        Although it wasn't until high school that I was able to discover the world of technology and programming, I've always 
+        Hi there! I'm Adithya, a computer science major minoring in finance <br></br>at the University of Illinois at Chicago.<br></br>
+        Although it wasn't until high school that I was able to discover <br></br>the world of technology and programming, <br></br>I've always 
         held a deep passion for the art of creating. 
       </p>
       </div>
